420e829344fe85e91c637b5acce7c9a1f0187c7b
David Whitesel <whiteseldavid@gmail.com>
2021-04-25 14:55:09 -0700

    Adding time if statement

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,33 @@
 //use moment() to display current date and time.
-var currentDay = moment();
-$("#currentDay").text(currentDay.format("MMM Do, YYYY"));
+var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").text(currentDay);
 
+function timeTracker() {
+    //Obtain current timestamps.
+    var timeNow = moment().hour();
+
+    // Loops thru all time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
 
 
