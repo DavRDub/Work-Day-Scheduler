f3f5bd01218d6ead9dca7ea4763ede7c91f62905
David Whitesel <whiteseldavid@gmail.com>
2021-04-25 17:49:54 -0700

    making commit

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,49 @@
-//use moment() to display current date and time.
-var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").text(currentDay);
+$(document).ready(function () {
+    
+    var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").text(currentDay);
 
-$(".saveBtn").on("click", function () {
-    //obtains closest values.
-    console.log(this);
-     // applies changes made to description class.
-    var text = $(this).siblings(".description").val();
-     // applies changes made to the time ids.
-    var time = $(this).parent().attr("id");
-
-    //set items in local storage.
-    localStorage.setItem(time, text);
-})
-
-function timeStamping() {
-    //Obtains current timestamps.
-    var currentTime = moment().hour();
-
-    // Loops thru all time-stamps
-    $(".time-stamp").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(blockTime); 
-
-        //Refers to the current time to run if statement.
-        if (blockTime < currentTime) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }
-        else if (blockTime === currentTime) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-
-        }
+    $(".saveBtn").on("click", function () {
+         // applies changes made to description class.
+        var text = $(this).siblings(".description").val();
+         // applies changes made to the time ids.
+        var time = $(this).parent().attr("id");
+    
+        //set items in local storage.
+        localStorage.setItem(time, text);
     })
-}
+    //retreives data from LocalStorage.
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
+    function timeStamping() {
+        var currentTime = moment().hour();
 
+        $(".time-stamp").each(function () {
+            var pauseTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (pauseTime < currentTime){
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            } else if (pauseTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+    timeStamping();
+})
